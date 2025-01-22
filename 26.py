609710bc445cf096b5ea49c3755f64cbca2df3ae
sajid6230 <sajid6230@gmail.com>
2025-01-22 13:36:49 -0600

    26th Problem

---
+++ 26.py
@@ -0,0 +1,9 @@
+"""
+26.	Write a Python program to find the index of an item in a tuple.
+
+"""
+a = (1,2,3,4,5,"A","B")
+
+for index, item in enumerate(a):
+    print("index:", index, "Item:", item)
+
