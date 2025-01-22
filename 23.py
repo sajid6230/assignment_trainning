551032980f5046b567c9ed39c680a24bdcdf31f5
sajid6230 <sajid6230@gmail.com>
2025-01-21 20:12:44 -0600

    23rd Problem

---
+++ 23.py
@@ -0,0 +1,13 @@
+"""
+23.	Write a Python program to unpack a tuple into several variables.
+"""
+
+tuple_1 = ("x","y","z")
+
+variable_1,variable_2,variable_3 = tuple_1
+
+
+print("variable_1 = ", variable_1)
+print("variable_2 = ", variable_2)
+print("variable_3 = ", variable_3)
+
